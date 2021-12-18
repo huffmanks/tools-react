1300eeb419def4a78ac0800daf236e3f1425f7ff
huffmanks <huffmanks2@gmail.com>
2021-12-17 23:18:33 -0500

    home links

--- src/components/utilities/HomeCards/index.js
+++ src/components/utilities/HomeCards/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
@@ -15,9 +17,9 @@ const HomeCards = () => {
             {routesArray.slice(1).map(({ key, path, name, icon, description }) => (
                 <Card className='card' key={key} sx={{ maxWidth: 345, padding: 3 }}>
                     <div className='card-icon'>
-                        <IconButton component='a' href={path} aria-label={name}>
-                            {icon}
-                        </IconButton>
+                        <Link to={path}>
+                            <IconButton aria-label={name}>{icon}</IconButton>
+                        </Link>
                     </div>
                     <CardContent>
                         <Typography gutterBottom variant='h5' component='div'>
@@ -26,19 +28,19 @@ const HomeCards = () => {
                         <Typography variant='body2'>{description}</Typography>
                     </CardContent>
                     <CardActions>
-                        <Button
-                            variant='contained'
-                            component='a'
-                            href={path}
-                            sx={{
-                                backgroundColor: '#8a6e4b',
-                                ':hover': {
-                                    backgroundColor: '#222',
-                                },
-                            }}
-                            fullWidth>
-                            View
-                        </Button>
+                        <Link className='card-view-link' to={path}>
+                            <Button
+                                variant='contained'
+                                sx={{
+                                    backgroundColor: '#8a6e4b',
+                                    ':hover': {
+                                        backgroundColor: '#222',
+                                    },
+                                }}
+                                fullWidth>
+                                View
+                            </Button>
+                        </Link>
                     </CardActions>
                 </Card>
             ))}
