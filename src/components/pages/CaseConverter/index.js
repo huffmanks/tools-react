7487ad55fd44c36ac997e3494064fc6bf77bdac9
huffmanks <huffmanks2@gmail.com>
2021-12-17 23:11:11 -0500

    case converter

--- src/components/pages/CaseConverter/index.js
+++ src/components/pages/CaseConverter/index.js
@@ -1,10 +1,31 @@
+import { useState } from 'react'
+
 import PageTitle from '../../layout/PageTitle'
 
+import CaseText from '../../utilities/CaseConverter/CaseText'
+import CaseButtons from '../../utilities/CaseConverter/CaseButtons'
+
 const CaseConverter = () => {
+    const [output, setOutput] = useState(null)
+
+    const handleUpdate = (e) => {
+        setOutput(e.target.value)
+    }
+
+    const handleCopy = () => {
+        return navigator.clipboard.writeText(output)
+    }
+
+    const handleDelete = () => {
+        setOutput('')
+    }
+
     return (
         <>
             <PageTitle>Case Converter</PageTitle>
-            <div>Case Converter page</div>
+
+            <CaseText output={output} handleUpdate={handleUpdate} handleCopy={handleCopy} handleDelete={handleDelete} />
+            <CaseButtons output={output} handleUpdate={handleUpdate} setOutput={setOutput} />
         </>
     )
 }
---
+++ src/components/utilities/CaseConverter/CaseText/index.js
@@ -0,0 +1,38 @@
+import FormControl from '@mui/material/FormControl'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+import './index.css'
+
+const CaseText = ({ output, handleUpdate, handleCopy, handleDelete }) => {
+    return (
+        <FormControl fullWidth className='form-control'>
+            <OutlinedInput
+                id='textarea'
+                inputComponent='textarea'
+                onChange={handleUpdate}
+                value={output}
+                endAdornment={
+                    <>
+                        <InputAdornment position='end'>
+                            <IconButton aria-label='copy value to clipboard' edge='end' onClick={handleCopy}>
+                                <ContentCopyIcon />
+                            </IconButton>
+                        </InputAdornment>
+
+                        <InputAdornment position='end'>
+                            <IconButton aria-label='delete value' edge='end' onClick={handleDelete}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    </>
+                }
+            />
+        </FormControl>
+    )
+}
+
+export default CaseText
